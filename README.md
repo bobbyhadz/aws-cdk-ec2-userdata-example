5f51f56bc786057c32264c2cd26aea66d3a85a1b
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-07 14:31:55 +0300

    added infrastructure code

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Article name here
+# EC2 User Data Example in AWS CDK - Complete Guide
 
 A repository for an article on
-[bobbyhadz.com](https://bobbyhadz.com/blog/aws-s3-presigned-url-react)
-`<- Update Link`
+[bobbyhadz.com](https://bobbyhadz.com/blog/aws-cdk-ec2-userdata-example)
 
 ## How to Use
 
@@ -14,7 +13,9 @@ A repository for an article on
 npm install
 ```
 
-3. Create the CDK stack
+3. Create a Key pair with the name of `ec2-key-pair` in your default region
+
+4. Create the CDK stack
 
 ```bash
 npx cdk deploy
--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,8 +1,50 @@
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
+import {readFileSync} from 'fs';
 
 export class CdkStarterStack extends cdk.Stack {
-  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, 'my-default-vpc', {
+      isDefault: true,
+    });
+
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+    });
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: webserverSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: 'ec2-key-pair',
+    });
+
+    // 👇 load user data script
+    const userDataScript = readFileSync('./lib/user-data.sh', 'utf8');
+    // 👇 add user data to the EC2 instance
+    ec2Instance.addUserData(userDataScript);
   }
 }
