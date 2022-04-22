499dee5163ed4bb16231e4aa40e5a30f44cea6e7
Borislav Hadzhiev <opmftw@gmail.com>
2022-04-22 20:05:37 +0300

    minor changes

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -27,7 +27,7 @@ export class CdkStarterStack extends cdk.Stack {
       },
       securityGroup: webserverSG,
       instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
+        ec2.InstanceClass.BURSTABLE2,
         ec2.InstanceSize.MICRO,
       ),
       machineImage: new ec2.AmazonLinuxImage({
